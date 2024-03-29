283572f6db64d418513e98a85f5305c2c5741d5d
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-03-29 11:47:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+# pip install virtualenv env (Install the Virtual Environment)
+
+# python -m virtualenv env (Move to env environment)
+
+# pip install flask (Install flask)
+
+# Goto app.py location and run : python -m flask run (Run the fFlask Application)
+
+from flask import Flask, render_template,abort
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
